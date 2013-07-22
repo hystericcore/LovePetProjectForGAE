61579877e0ab4c3f3fe102325c13bc68e7c0873f
hystericcore <hystericcore@gmail.com>
2013-07-22 20:52:15 +0900

    Add /todayPetList API for send parsed data to baas.io

--- src/com/lovepetproject/LovePetProjectController.java
+++ src/com/lovepetproject/LovePetProjectController.java
@@ -1,8 +1,17 @@
 package com.lovepetproject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DecimalFormat;
 import java.util.*;
 
 import net.htmlparser.jericho.Element;
@@ -12,36 +21,117 @@ import net.htmlparser.jericho.Source;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson;
 
 @Controller
 public class LovePetProjectController {
 	static String domain = "http://animal.go.kr";
+	static String baasiourl = "https://api.baas.io/19afa818-e241-11e2-9011-06530c0000b4/3a653992-e241-11e2-9011-06530c0000b4/pets";
 	
-	@RequestMapping(value="/petList", method=RequestMethod.GET) 
-	public ModelAndView petList(
-			@RequestParam(value="startDate", required=false) String startDate, 
-			@RequestParam(value="endDate", required=false) String endDate, 	
-			@RequestParam(value="petKind", required=false) String petKind, 
-			@RequestParam(value="pageCount", required=false) String pageCount,
-			@RequestParam(value="location", required=false) String location) throws MalformedURLException, IOException {
+	@RequestMapping(value="/todayPetList", method=RequestMethod.GET)
+	public ModelAndView todayPetQuery() throws MalformedURLException, IOException, NoSuchAlgorithmException{
+		// parameter
+		String date = getCurrentDate();
+		int pageCount = 1;
+		boolean loading = true;
 		
+		// container
+		List<PetVO> todayPetList = new ArrayList<PetVO>();
+		
+		// query
+		while (loading) {
+			List<PetVO> petList = getPetList(date, pageCount);
+			
+			if (petList == null) {
+				loading = false;
+			} else {
+				todayPetList.addAll(petList);
+				pageCount++;
+			}
+		}
+		
+		String petNameList = getPetNameListFromBaas(date);
+		
+		for (PetVO petVO : todayPetList) {
+			String encodedID = encodeString(petVO.getBoardID());
+			
+			if (petNameList.contains(encodedID) != true) {
+				String json = new Gson().toJson(petVO);
+				putPetDataToBaas(petVO.getBoardID(), json);
+			}
+		}
+		
+		return new ModelAndView("todayPetList", "todayPetList", todayPetList);
+	}
+	
+	private String getPetNameListFromBaas(String date) throws IOException {
+		String sql = "?ql=select%20name%20where%20date%3D'" + date + "'&limit=100";
+		URL baasUrl = new URL(baasiourl + sql);
+		
+		System.out.println(baasiourl + sql);
+		
+		HttpURLConnection conn = (HttpURLConnection) baasUrl.openConnection();
+		conn.setRequestMethod("GET");
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+        
+		return response.toString();
+	}
+
+	private void putPetDataToBaas(String boardID, String json) throws MalformedURLException,
+			IOException, ProtocolException, UnsupportedEncodingException, NoSuchAlgorithmException {
+		String encodedID = encodeString(boardID);
+        
+		// baas.io write
+		URL baasUrl = new URL(baasiourl + "/" + encodedID);
+		HttpURLConnection conn = (HttpURLConnection) baasUrl.openConnection();
+		
+		//add request header
+		conn.setRequestMethod("PUT");
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Content-Length", ""+ json.length());
+		
+		// Send post request
+		conn.setDoOutput(true);
+		OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+		wr.write(json);
+		wr.flush();
+		wr.close();
+		
+//		int responseCode = conn.getResponseCode();
+//		System.out.println(responseCode + " : " + response.toString());
+ 
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+	}
+	
+	private List<PetVO> getPetList(String date, int pageCount) throws MalformedURLException, IOException {
+		// container
+		List<PetVO> petList = new ArrayList<PetVO>();
+		
+		// query
 		String url = domain + "/portal_rnl/abandonment/protection_list.jsp?"
-		+ (startDate != null ? ("s_date=" + startDate + "&") : "")
-		+ (endDate != null ? ("e_date=" + endDate + "&") : "")
-		+ (petKind != null ? ("s_up_kind_cd=" + petKind + "&") : "")
-		+ (pageCount != null ? ("pagecnt=" + pageCount + "&") : "")
-		+ (location != null ? ("s_upr_cd=" + location + "&s_org_cd=0000000") : "");
-		
+				+ "s_date=" + date + "&e_date=" + date + "&pagecnt=" + pageCount;
 		Source source = new Source(new URL(url));
 		
 		List<Element> thumbnailImgNodes = source.getAllElementsByClass("thumbnail_img01");
 		List<Element> linkButtonNodes = source.getAllElementsByClass("thumbnail_btn01");
-		List<Element> tableNodes = source.getAllElementsByClass("thumbnail_table01");
-		
-		List<PetVO> petList = new ArrayList<PetVO>();
-		
+
 		for (int i = 0; i < thumbnailImgNodes.size(); i++) {
 			PetVO vo = new PetVO();
 			
@@ -53,52 +143,80 @@ public class LovePetProjectController {
 			Element linkNode = linkButtonNode.getFirstElement(HTMLElementName.A);
 			vo.setLinkSrc(domain + linkNode.getAttributeValue("href"));
 			
-			Element tableNode = tableNodes.get(i);
-			List<Element> tdNodes = tableNode.getAllElements(HTMLElementName.TD);
-			vo.setBoardID(tdNodes.get(0).getContent().getTextExtractor().toString());
-			vo.setDate(tdNodes.get(1).getContent().getTextExtractor().toString());
-			vo.setType(tdNodes.get(2).getContent().getTextExtractor().toString());
-			vo.setSex(tdNodes.get(3).getContent().getTextExtractor().toString());
-			vo.setFoundLocation(tdNodes.get(4).getContent().getTextExtractor().toString());
-			vo.setDetail(tdNodes.get(5).getContent().getTextExtractor().toString());
-			vo.setState(tdNodes.get(6).getContent().getTextExtractor().toString());
-			
 			petList.add(vo);
 		}
 		
-		return new ModelAndView("petList", "petList", petList);
+		if (0 == petList.size()) {
+			return null;
+		}
+		
+		for (int i = 0; i < petList.size(); i++) {
+			PetVO vo = petList.get(i);
+			source = new Source(new URL(vo.getLinkSrc()));
+			
+			// Image
+			Element imageNode = source.getFirstElementByClass("photoArea");
+			vo.setImageSrc(domain + imageNode.getAttributeValue("src"));
+			
+			// Text
+			Element tableNode = source.getFirstElementByClass("viewTable");
+			List<Element> trNodes = tableNode.getAllElements(HTMLElementName.TR);
+			vo.setBoardID(trNodes.get(0).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			vo.setPetType(trNodes.get(1).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			vo.setColor(trNodes.get(2).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			vo.setSex(trNodes.get(3).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			String yearAndWeight = trNodes.get(4).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString();
+			String[] values = yearAndWeight.split("/");
+			vo.setYear(values[0].trim());
+			vo.setWeight(values[1].trim());
+			vo.setFoundLocation(trNodes.get(5).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			vo.setDate(trNodes.get(6).getAllElements(HTMLElementName.TD).get(0).getContent().getTextExtractor().toString());
+			// vo.setNeutralize(trNodes.get(6).getAllElements(HTMLElementName.TD).get(1).getContent().getTextExtractor().toString());
+			vo.setDetail(trNodes.get(7).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			vo.setDistrictOffice(trNodes.get(9).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			vo.setState(trNodes.get(10).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			vo.setCenterName(trNodes.get(11).getAllElements(HTMLElementName.TD).get(0).getContent().getTextExtractor().toString());
+			vo.setCenterTel(trNodes.get(11).getAllElements(HTMLElementName.TD).get(1).getContent().getTextExtractor().toString());
+			vo.setCenterLocation(trNodes.get(12).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
+			
+			petList.set(i, vo);
+		}
+		
+		return petList;
 	}
 	
-	@RequestMapping(value="/petDetail", method=RequestMethod.POST) 
-	public ModelAndView petDetail(@RequestParam(value="linkSrc", required=true) String linkSrc) throws MalformedURLException, IOException {
-		Source source = new Source(new URL(linkSrc));
-		PetDetailVO vo = new PetDetailVO();
+	/**
+	  * @return YYYY-MM-DD 형식의 오늘 날짜
+	  */
+	private static String getCurrentDate(){
+		 DecimalFormat df = new DecimalFormat("00");
+		 Calendar currentCal = Calendar.getInstance();
+	  
+		 currentCal.add(currentCal.DATE, 0);
+	  
+		 String year = Integer.toString(currentCal.get(Calendar.YEAR));
+		 String month = df.format(currentCal.get(Calendar.MONTH)+1);
+		 String day = df.format(currentCal.get(Calendar.DAY_OF_MONTH));
+	  
+		 return year+"-"+month+"-"+day;
+	}
+	
+	
+	private String encodeString(String string) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		// encode
+		StringBuffer hexString = new StringBuffer();
+		byte[] bytes = string.getBytes("UTF-8");
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte[] hash = md.digest(bytes);
 		
-		// Image
-		Element imageNode = source.getFirstElementByClass("photoArea");
-		vo.setImageSrc(domain + imageNode.getAttributeValue("src"));
-		
-		// Text
-		Element tableNode = source.getFirstElementByClass("viewTable");
-		List<Element> trNodes = tableNode.getAllElements(HTMLElementName.TR);
-		vo.setBoardID(trNodes.get(0).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		vo.setType(trNodes.get(1).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		vo.setColor(trNodes.get(2).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		vo.setSex(trNodes.get(3).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		String yearAndWeight = trNodes.get(4).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString();
-		String[] values = yearAndWeight.split("/");
-		vo.setYear(values[0].trim());
-		vo.setWeight(values[1].trim());
-		vo.setFoundLocation(trNodes.get(5).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		vo.setDate(trNodes.get(6).getAllElements(HTMLElementName.TD).get(0).getContent().getTextExtractor().toString());
-		// vo.setNeutralize(trNodes.get(6).getAllElements(HTMLElementName.TD).get(1).getContent().getTextExtractor().toString());
-		vo.setDetail(trNodes.get(7).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		vo.setDistrictOffice(trNodes.get(9).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		vo.setState(trNodes.get(10).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		vo.setCenterName(trNodes.get(11).getAllElements(HTMLElementName.TD).get(0).getContent().getTextExtractor().toString());
-		vo.setCenterTel(trNodes.get(11).getAllElements(HTMLElementName.TD).get(1).getContent().getTextExtractor().toString());
-		vo.setCenterLocation(trNodes.get(12).getFirstElement(HTMLElementName.TD).getContent().getTextExtractor().toString());
-		
-		return new ModelAndView("petDetail", "petDetail", vo);
+		for (int i = 0; i < hash.length; i++) {
+            if ((0xff & hash[i]) < 0x10) {
+                hexString.append("0" + Integer.toHexString((0xFF & hash[i])));
+            } else {
+                hexString.append(Integer.toHexString(0xFF & hash[i]));
+            }
+        }
+
+		return hexString.toString();
 	}
 }
--- src/com/lovepetproject/PetVO.java
+++ src/com/lovepetproject/PetVO.java
@@ -1,15 +1,28 @@
 package com.lovepetproject;
 
 public class PetVO {
+	// list view
 	private String thumbnailSrc;
 	private String linkSrc;
+	
+	// detail view
 	private String boardID;
-	private String date;
-	private String type;
+	private String petType;
 	private String sex;
 	private String foundLocation;
+	private String date;
 	private String detail;
 	private String state;
+	private String imageSrc;
+	private String color;
+	private String year;
+	private String weight;
+	private String neutralize;
+	private String districtOffice;
+	private String centerName;
+	private String centerTel;
+	private String centerLocation;
+	
 	public String getThumbnailSrc() {
 		return thumbnailSrc;
 	}
@@ -34,11 +47,11 @@ public class PetVO {
 	public void setDate(String date) {
 		this.date = date;
 	}
-	public String getType() {
-		return type;
+	public String getPetType() {
+		return petType;
 	}
-	public void setType(String type) {
-		this.type = type;
+	public void setPetType(String petType) {
+		this.petType = petType;
 	}
 	public String getSex() {
 		return sex;
@@ -63,5 +76,59 @@ public class PetVO {
 	}
 	public void setState(String state) {
 		this.state = state;
+	}
+	public String getImageSrc() {
+		return imageSrc;
+	}
+	public void setImageSrc(String imageSrc) {
+		this.imageSrc = imageSrc;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getNeutralize() {
+		return neutralize;
+	}
+	public void setNeutralize(String neutralize) {
+		this.neutralize = neutralize;
+	}
+	public String getDistrictOffice() {
+		return districtOffice;
+	}
+	public void setDistrictOffice(String districtOffice) {
+		this.districtOffice = districtOffice;
+	}
+	public String getCenterName() {
+		return centerName;
+	}
+	public void setCenterName(String centerName) {
+		this.centerName = centerName;
+	}
+	public String getCenterTel() {
+		return centerTel;
+	}
+	public void setCenterTel(String centerTel) {
+		this.centerTel = centerTel;
+	}
+	public String getCenterLocation() {
+		return centerLocation;
+	}
+	public void setCenterLocation(String centerLocation) {
+		this.centerLocation = centerLocation;
 	}
 }
